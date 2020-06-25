2c9bc01186eb8dfd122dc721f5d357e117ad4c5e
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-06-25 22:51:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,10 @@ if __name__ == '__main__':
 ##############   set envirnment variable for secret key   #############
 
 
-# REDESIGN REGISTER.HTML FOR NEW FORM TYPE WITH CLASSES 			--- done
-# REDESIGN LOGIN.HTML FOR NEW FORM TYPE WITH CLASSES				--- done
 # Add password strength validators later to user/form.py
-# show validation messages in login.html and register.html			--- done
-# Flask nicer messages 												--- done
 # work on putting everything together
 # implement password reset functionality with firebase
-# show error messages in form fields								--- done
 # email verification with firebase
-# remember me functionality											--- done
-# accept terms and service functionality							--- done
 # google OAuth
 # facebook OAuth
 # prettify contact us page
