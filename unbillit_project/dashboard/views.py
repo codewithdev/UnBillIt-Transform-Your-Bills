6f64b3c436f4da7723083ec5a4443b2adeeaec32
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-06-25 22:45:46 +0530

    added dashboard view files

---
+++ unbillit_project/dashboard/views.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect, url_for, flash, Blueprint, request
+from flask_login import (login_user, login_required, current_user, logout_user)
+from unbillit_project import db
+from unbillit_project.models import User
+from unbillit_project.user.forms import RegistrationForm, LoginForm, UpdateUserForm
+from unbillit_project.user.pic_handler import add_profile_pic
+from werkzeug.security import generate_password_hash, check_password_hash
+
+dashboard_views = Blueprint('dashboard', __name__)
+
+@dashboard_views.route('/welcome')
+@login_required
+def welcome():
+	return render_template('welcome.html')
