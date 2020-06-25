c25b211f4082e44b779e5933703d415d5fa227a9
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-06-25 22:03:24 +0530

    Add files via upload

---
+++ unbillit_project/models.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from unbillit_project import db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    profile_img = db.Column(db.String(64), nullable=False, default='default_profile.png')
+    first_name = db.Column(db.String(24), nullable=False)
+    last_name = db.Column(db.String(24), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False, index=True)
+    password = db.Column(db.String(256))
+
+    def __init__(self, first_name, last_name, email, password):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
